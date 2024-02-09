06c2c9973c8beeeca38aa4fd984fd54ef4cd255a
Scott Feichter <scottfeichter@gmail.com>
2024-02-08 18:30:13 -0800

    conversion passing

--- problems/binary-to-string.js
+++ problems/binary-to-string.js
@@ -1,10 +1,26 @@
 function binaryToString(binaryBlob) {
-  // Your code here 
+
+  let bits = [];
+
+  for (let i = 0; i < binaryBlob.length; i+= 8) {
+    bits.push(binaryBlob.slice(i, i + 8));
+  }
+
+  for (let i = 0; i < bits.length; i++) {
+    bits[i] = String.fromCharCode(parseInt(bits[i], 2));
+  }
+
+
+
+
+
+
+  return bits.join("");
 }
 
 /* Comment in the code below to run local tests */
-// console.log(binaryToString('010000010100001001000011')); // 'ABC'
-// console.log(binaryToString('001101100011011100111000')); // '678'
+console.log(binaryToString('010000010100001001000011')); // 'ABC'
+console.log(binaryToString('001101100011011100111000')); // '678'
 
 
 module.exports = binaryToString;
